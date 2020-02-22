64d43d20c4011c9b9e72725912b61894fd0d736b
Thanos Pasias <thanospas@yahoo.com>
2020-02-22 23:34:12 +0100

    Optimized PCA + heatmap

--- SA_Heart_disease_PCA_heatmap.py
+++ SA_Heart_disease_PCA_heatmap.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 22 19:43:58 2020
+Created on Sat Feb 22 21:54:03 2020
 
 @author: white
 """
@@ -17,7 +17,7 @@ historyDict = dict(zip(fam_history,[0,1]))
 y = np.array([historyDict[cl] for cl in fam_history])
 binary_heart_data= heart_data.copy()
 binary_heart_data.famhist = y    
-
+binary_heart_data.drop('row.names', axis=1, inplace=True)
 from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
@@ -33,9 +33,10 @@ x_pca = pca.transform(scaled_data)
 
 plt.figure(figsize=(8,6))
 plt.scatter(x_pca[:,2],x_pca[:,4],c=binary_heart_data.famhist,cmap='plasma')
-plt.xlabel('Fourth principal component')
+plt.xlabel('Third principal component')
 plt.ylabel('Fifth Principal Component')
 
+df_comp = pd.DataFrame(pca.components_,columns=binary_heart_data.columns)
+plt.figure(figsize=(12,6))
+sns.heatmap(df_comp,cmap='plasma',)
 plt.show()
-
-#
